a4e996ce0e647ac0d1cd4d2326f1a56904a37f82
Danara Sarioglu <ds4489@nyu.edu>
2018-10-30 13:48:18 -0400

    first commit

---
+++ htmlscripting.py
@@ -0,0 +1,52 @@
+import csv
+
+htmlBeginning = """
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Postmodern Times Script</title>
+  <meta name="description" content="Postmodern Times Script">
+  <meta name="author" content="MM">
+
+  <style>
+		body {
+			font-family: courier;
+		}
+		.titles{
+			text-align: left;
+			margin-left: 1in;
+			margin-bottom: 0.3in;
+		}
+		.fade {
+			text-align: left;
+			margin-bottom: 0.3in;
+
+			<!-- text-translate: uppercase; -->
+		}
+  </style>
+
+</head>
+
+<body>
+"""
+
+Html_file= open("scripts.html","w")
+Html_file.write(htmlBeginning)
+with open('scenes.csv', mode='r') as csv_file:
+	csv_reader = csv.reader(csv_file)
+	for row in csv_reader:
+		div = '<div class="%s">%s</div>'+'\n'
+		info = list(row)
+		if info[0]=='fade':
+			info[1]=info[1].upper()
+		print (div %(info[0],info[1]))
+		Html_file.write((div %(info[0],info[1])))
+htmlEnd = """
+</body>
+</html>
+"""
+Html_file.write(htmlEnd)
+Html_file.close()
